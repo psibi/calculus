65e0910cd359801d800468bc1099ae1344757a3e
Sibi Prabakaran <sibi@psibi.in>
2020-11-27 11:00:12 +0530

    Update site

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,10 +4,10 @@
 ((org-mode
   (org-twbs-link-up . "https://psibi.in/velleman-calculus/")
   (org-twbs-link-home . "https://psibi.in/")
-  (org-publish-project-alist . '("velleman-calculus" 
+  (org-publish-project-alist . (("velleman-calculus" 
                                   :base-directory "~/github/velleman-calculus/"
                                   :recursive t
                                   :base-extension "org"
                                   :auto-sitemap t
                                   :publishing-function org-twbs-publish-to-html
-                                  :publishing-directory "~/github/velleman-calculus/docs/"))))
+                                  :publishing-directory "~/github/velleman-calculus/docs/")))))
